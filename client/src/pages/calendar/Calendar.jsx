9fee5d4c29e107be41e45d2db345834dc57fe510
Maku <mkaptowka@gmail.com>
2023-08-23 12:03:26 +0600

    f

--- client/src/pages/calendar/Calendar.jsx
+++ client/src/pages/calendar/Calendar.jsx
@@ -3,44 +3,78 @@ import React, { useState, useEffect } from 'react';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from "@fullcalendar/interaction"
+
 import './Calendar.scss'
 
 function CalendarPage(props) {
-
-    const today = '';
-    const year = 2023;
-    const monthes = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-    
     const [ events, setEvents ] = useState([
         {
-            'day': 0,
-            'month': 1,
-            'year': 2023,
+            'id': 0,
+            'date': '',
             'content': '',
+            'title': '',
             'type': ''
         }
     ]);
 
+    const [showEvent, setShowEvent] = useState(false);
+    const [currEvent, setCurrEvent] = useState({
+        'id': 0,
+        'date': '',
+        'content': '',
+        'title': '',
+        'type': ''
+    });
+
     useEffect(() => {
         setEvents([
             {
-                'day': 21,
-                'month': 9,
-                'year': 2023,
+                'id': 1,
+                'date': '2023-09-21',
+                '_date': '2023-09-21',
                 'content': 'Всеобщее декларирование: Особенности заполнения заполнения заполнения заполнения заполнения заполнения заполнения заполнения заполнения заполнения формы 250.00 и 270.00 за 2023 год',
-                'type': 'Вебинаp'
+                'title': 'Вебинаp',
+                'type': 'Вебинаp',
+
             },
             {
-                'day': 21,
-                'month': 9,
-                'year': 2023,
-                'content': 'Всеобщее декларирование: Особенности заполнения формы 250.00 и 270.00 за 2023 год',
+                'id': 2,
+                'date': '2023-08-24',
+                '_date': '2023-08-24',
+                'content': 'Всеобщее декларирование: Особенности заполнения заполнения заполнения заполнения заполнения заполнения заполнения заполнения заполнения заполнения формы 250.00 и 270.00 за 2023 год',
+                'title': 'Вебинаp',
+                'type': 'Вебинаp',
+
+            },
+            {
+                'id': 3,
+                'date': '2023-09-21',
+                '_date': '2023-09-21',
+                'content': 'Всеобщее декларирование: Особенности заполнения заполнения заполнения заполнения заполнения заполнения заполнения заполнения заполнения заполнения формы 250.00 и 270.00 за 2023 год',
+                'title': 'Вебинаp',
                 'type': 'Вебинаp'
             }
         ])
     }, [])
 
-    console.log(today);
+    const handleDateClick = (arg) => {
+        // console.log(arg, typeof arg)
+    }
+
+    const handleEventClick = (arg) => {
+        console.log(arg.event.extendedProps);
+
+        let id = arg.event.id - 0
+
+        setCurrEvent(events.filter(item => {
+          return item.id === id
+        })[0]);
+
+        setShowEvent(true);
+    }
 
     return ( 
         <>
@@ -52,25 +86,42 @@ function CalendarPage(props) {
                     <h2>Календарь мероприятий</h2>
 
                     <div className="calendar-wrapper">
-                        <h3>{today}</h3>
+                        {/* <h3>{today}</h3> */}
 
                         <div className="calendar-body">
+                            <FullCalendar
+                                locale={'ru-RU'}
+                                buttonText={{
+                                    today: "Сегодня",
+                                    month: "Месяц",
+                                    week: "Неделя",
+                                    day: "День",
+                                    list: "Список"
+                                }}
+                                headerToolbar={{
+                                    left: "title",
+                                    center: "",
+                                    right: "prev,today,next"
+                                }}
+                                plugins={[ dayGridPlugin, interactionPlugin ]}
+                                initialView="dayGridMonth"
+                                selectable={true}
+                                events={events}
+                                dateClick={handleDateClick}
+                                eventContent={renderEventContent}
+                                eventClick={handleEventClick}
+
+                            />
                         </div>
 
                         <div className="calendar-events">
                             {
-                                events.map((event) => (
-                                    <div className='event-card'>
-                                        <div>
-                                            <p>{event.day} {monthes[event.month]}</p>
-                                            <p>{event.year}</p>
-                                        </div>
-                                        <div>
-                                            <p>{event.content}</p>
-                                            <div className={'event-type'}>{event.type}</div>
-                                        </div>
-                                    </div>
-                                ))
+                                !showEvent ? 
+                                    events.map((event) => {
+                                        <EventCard event={event}/>
+                                    })
+                                :
+                                    <EventCard event={currEvent}/>
                             }
                         </div>
                     </div>
@@ -81,5 +132,39 @@ function CalendarPage(props) {
         </>
     );
 }
+
+const EventCard = ({event}) => {
+    const monthes = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+
+    let day = event.date.substring(8);
+    let month = event.date.substring(5, 7);
+    month = monthes[parseInt(month) - 1];
+    let year = event.date.substring(0, 4)
+
+    return (
+        <div className='event-card'>
+            <div>
+                <p>{day} {month}</p>
+                <p>{year}</p>
+            </div>
+            <div>
+                <p>{event.content}</p>
+                <div className={'event-type'}>{event.title}</div>
+            </div>
+        </div>
+    )
+}
+
+const renderEventContent = (eventInfo) => {
+    console.log("event", eventInfo);
+
+    return (
+      <>
+        <div style={{cursor: 'pointer', background: '#F9CB36', border: 'none', padding: '8px'}}>
+            <i style={{color: 'black'}}>{eventInfo.event.title}</i>
+        </div>
+      </>
+    )
+  }
 
 export default CalendarPage;
