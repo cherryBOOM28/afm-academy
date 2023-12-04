cd9a20bcd9190026dc249257256b34fc5dda6894
Maku <mkuanyshbekov@list.com>
2023-12-04 18:36:26 +0600

    f

--- client/src/components/courseTemplates/complex/DragAndDropTwoSide/index.jsx
+++ client/src/components/courseTemplates/complex/DragAndDropTwoSide/index.jsx
@@ -1,77 +1,129 @@
 import React, { useState } from 'react';
 import './style.scss'
-
-const QuestionCard = ({ answer, side }) => {
-  const dragStart = (event) => {
-    console.log("Started drag")
-    event.dataTransfer.setData('text/plain', JSON.stringify({ answer, side }));
-  };
-
-  return (
-    <div
-      draggable="true"
-      onDragStart={dragStart}
-      className='question-card'
-    >
-      {answer}
-    </div>
-  );
-};
+import { motion, useAnimation, whileHover } from 'framer-motion';
 
 const QuestionContainer = ({ questions, leftAnswer, rightAnswer }) => {
-  const [leftSide, setLeftSide] = React.useState([]);
-  const [rightSide, setRightSide] = React.useState([]);
+  const mainControls = useAnimation();
+  const variants = {
+    initial: {
+      borderColor: 'rgba(0, 0, 0)',
+    },
+    correct: {
+      borderColor: 'rgba(110, 245, 153)',
+    },
+    wrong: {
+      borderColor: 'rgba(227, 129, 129)',
+    },
+    hover: {
+      borderColor: 'rgba(54, 162, 245)',
+    }
+  }
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
 
-  const allowDrop = (event) => {
-    event.preventDefault();
+  const handleOnDragStart = (e, side, answer) => {
+    e.dataTransfer.setData('text/plain', JSON.stringify({ answer, side }));
   };
 
-  const handleDrop = (event, side) => {
-    event.preventDefault();
-    const data = JSON.parse(event.dataTransfer.getData('text/plain'));
-    if (side === 'left') {
-      setLeftSide([...leftSide, data]);
-    } else if (side === 'right') {
-      setRightSide([...rightSide, data]);
+  const handleOnDragOver = (e) => {
+    e.preventDefault(); // Prevent default behavior to enable dropping
+    
+    console.log('Dragging over the target');
+    e.target.style.cursor = 'pointer';
+    e.target.style.transform = 'scale(105%, 105%)';
+    mainControls.start('hover');
+  };
+
+  const handleOnDragLeave = (e) => {
+    e.preventDefault(); // Prevent default behavior
+    
+    console.log('Drag left the target');
+    setInitialStyle(e);
+  };
+
+  const handleOnDrop = (e, dropSide) => {
+    e.preventDefault();
+    
+    const data = e.dataTransfer.getData('text/plain');
+    const { answer, side } = JSON.parse(data);
+
+    console.log(side, answer, dropSide);
+
+    if (dropSide === side) {
+      setCorrectStyle(e);
+    } else {
+      setWrongStyle(e);
     }
+    // Handle the dropped data (answer, side)
+    // console.log(`Dropped: ${answer} on ${side} side`);
+    // Perform further actions based on the dropped data
+    setTimeout(() => {
+      setInitialStyle(e);
+    }, 1000);
+
+    // setInitialStyle(e);
   };
 
-  const leftDrop = (event) => handleDrop(event, 'left');
-  const rightDrop = (event) => handleDrop(event, 'right');
+  const setInitialStyle = (e) => {
+    e.target.style.cursor = 'initial';
+    e.target.style.transform = 'scale(100%, 100%)';
+    mainControls.start('initial');
+  }
+
+  const setWrongStyle = (e) => {
+    e.target.style.cursor = 'initial';
+    e.target.style.transform = 'scale(110%, 110%)';
+    mainControls.start('wrong');
+
+  }
+
+  const setCorrectStyle = (e) => {
+    e.target.style.cursor = 'initial';
+    e.target.style.transform = 'scale(110%, 110%)';
+    mainControls.start('correct');
+
+  }
 
   return (
     <div className='dnd-question-container'>
+      
       <div className='questions'>
-        {/* {questions.map((q, index) => ( */}
-          <QuestionCard answer={questions[currentQuestion].answer} side={questions[currentQuestion].side} />
-        {/*  */}
+        <div 
+          className="question-card" 
+          draggable
+          onDragStart={(e) => handleOnDragStart(e, 'Left', 'Question 1')}
+        >
+          {'The question 1'}
+        </div>
       </div>
       <div className="two-sides">
-        <div className='left'>
-          <div
-            onDrop={leftDrop}
-            onDragOver={allowDrop}
-          >
-            {/* {leftSide.map((card, index) => (
-              <QuestionCard key={index} answer={card.answer} side={card.side} />
-            ))} */}
-          </div>
-          <p>{leftAnswer}</p>
-        </div>
-        <div className='right'>
-          <div
-            onDrop={rightDrop}
-            onDragOver={allowDrop}
-            
-          >
-            {/* {rightSide.map((card, index) => (
-              <QuestionCard key={index} answer={card.answer} side={card.side} />
-            ))} */}
-          </div>
-          <p>{rightAnswer}</p>
-        </div>
+        <motion.div className='left'
+          animate={mainControls}
+          transition={{ duration: 0.7, ease: 'easeInOut' }}
+          variants={variants}
+          onDragOver={(e) => handleOnDragOver(e)}
+          onDragLeave={(e) => handleOnDragLeave(e)}
+          onDrop={(e) => handleOnDrop(e, 'Left')}
+        >
+          {
+            'Left Answer'
+          }
+        </motion.div>
+        <motion.div 
+          className='right' 
+          animate={mainControls}
+          transition={{ duration: 0.7, ease: 'easeInOut' }}
+          initial='initial'
+          variants={variants}
+          onDragOver={(e) => handleOnDragOver(e)}
+          onDragLeave={(e) => handleOnDragLeave(e)}
+          onDrop={(e) => handleOnDrop(e, 'Right')}
+          // whileHover={{ scale: 1.2, rotate: 90 }}
+        >
+          {
+            'Right Answer'
+          }
+        </motion.div>
       </div>
     </div>
   );
