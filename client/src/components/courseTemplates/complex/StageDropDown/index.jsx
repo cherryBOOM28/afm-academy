def885cee483fab87ee11a191b6a1af3693644e7
Maku <mkuanyshbekov@list.com>
2023-12-21 14:27:11 +0600

    f

--- client/src/components/courseTemplates/complex/StageDropDown/index.jsx
+++ client/src/components/courseTemplates/complex/StageDropDown/index.jsx
@@ -4,10 +4,17 @@ import './style.scss';
 import stage_1 from './static/stage_1.svg'
 import stage_2 from './static/stage_2.svg'
 import stage_3 from './static/stage_3.svg'
-import { AiOutlinePlus } from 'react-icons/ai';
+import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
+import { IoClose } from "react-icons/io5";
+
+import { motion } from "framer-motion"
 
 function StageDropDown({
-
+    stages=[
+        {icon: stage_1, text: 'Ст. 165 УК КазССР', innerText: 'Использование денежных средств и иного имущества, приобретенных или добытых преступным путем, для занятия предпринимательской деятельностью или иной не запрещенной законом деятельностью'},
+        {icon: stage_2, text: 'Ст. 193 УК РК', innerText: 'Легализация денежных средств или иного имущества, проиобретенного незаконным путем'},
+        {icon: stage_3, text: 'Ст. 218 УК РК', innerText: 'Легализация (отмывание) денег и (или) иного имущества, полученных преступным путем'},
+    ],
 }) {
     return ( 
         <div className="stage-dropdown">
@@ -15,68 +22,67 @@ function StageDropDown({
 
                 <div className="body">
 
-                    <div>
-                        <div className="icon">
-                            <img src={stage_1} alt="stage 1" />
-                        </div>
-                        <div className="line"></div>
-                        <div className="open-icon">
-                        {/* {
-                            open  */}
-                                {/* ? <AiOutlineMinus style={{color: color}} size={25} />  */}
-                                <AiOutlinePlus size={25}/>
-                        {/* } */}
-                        </div>
-                        <div className="text">
-                            Ст. 165 УК КазССР
-                        </div>
-
-                        <div className="inner">
-                            <div>
-                                Использование денежных средств и иного имущества, приобретенных или добытых преступным путем, для занятия предпринимательской деятельностью или иной не запрещенной законом деятельностью
-                            </div>
-                        </div>
-                    </div>
-
-                    <div>
-                        <div className="icon">
-                            <img src={stage_2} alt="stage 2" />
-                        </div>
-                        <div className="line"></div>
-                        <div className="open-icon">
-                        {/* {
-                            open  */}
-                                {/* ? <AiOutlineMinus style={{color: color}} size={25} />  */}
-                                <AiOutlinePlus size={25}/>
-                        {/* } */}
-                        </div>
-                        <div className="text">
-                            Ст. 165 УК КазССР
-                        </div>
-                    </div>
-
-                    <div>
-                        <div className="icon">
-                            <img src={stage_3} alt="stage 3" />
-                        </div>
-                        <div className="line"></div>
-                        <div className="open-icon">
-                        {/* {
-                            open  */}
-                                {/* ? <AiOutlineMinus style={{color: color}} size={25} />  */}
-                                <AiOutlinePlus size={25}/>
-                        {/* } */}
-                        </div>
-                        <div className="text">
-                            Ст. 165 УК КазССР
-                        </div>
-                    </div>
+                    {
+                        stages && stages.map((item, index) => (
+                            <Item 
+                                key={index}
+                                icon={item.icon}
+                                text={item.text}
+                                innerText={item.innerText}
+                            />
+                        ))
+                    }
 
                 </div>
 
             </div>
         </div>
     );
+}
+
+
+const Item = ({ icon, text, innerText }) => {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <div>
+            <div className="icon">
+                <img src={icon} alt="stage 1" />
+            </div>
+            <div className="line"></div>
+            <div className="open-icon">
+            {
+                open 
+                    ? <AiOutlineMinus size={25} />
+                    : <AiOutlinePlus size={25} onClick={() => setOpen(true)}/>
+            }
+            </div>
+            <div className="text">
+                { text }
+            </div>
+
+            <motion.div 
+                className="inner"
+                initial={'close'}
+                variants={{
+                    open: {left: '0px'},
+                    close: {left: '-100vw'}
+                }}
+                animate={open ? 'open' : 'close'}
+                transition={{
+                    
+                }}
+            >
+                <div className="close" onClick={() => setOpen(false)}>
+                    <IoClose size={23} />
+                </div>
+                <div className='inner-header'>{ text }</div>
+                <div className='inner-text'>
+                    { innerText }
+                </div>
+            </motion.div>
+        </div>
+    )
 }
 
 export default StageDropDown;
