d6d7fbb05dcdea452c640e0f32df53213d98ae5a
daulet <dauka2707@gmail.com>
2024-02-08 13:05:21 +0600

    Message

--- client/src/components/courseTemplates/common/ComplexTable/index.jsx
+++ client/src/components/courseTemplates/common/ComplexTable/index.jsx
@@ -1,54 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.scss';
 
-const ComplexTable = ({columns,data}) => {
+const ComplexTable = ({ columns, data }) => {
+    const [isCollapsed, setIsCollapsed] = useState(true);
+    const [showRows, setShowRows] = useState(5); // Начальное количество отображаемых строк
 
+    const toggleCollapse = () => {
+        setIsCollapsed(prevState => !prevState);
+        if (isCollapsed) {
+            setShowRows(data.length); // Показываем все строки при развертывании
+        } else {
+            setShowRows(5); // Показываем только 5 строк при сворачивании
+        }
+    };
 
-  return (
-    <div className='divka'>
-    <table className="ComplexTable">
-      <thead>
-        <tr>
-          {columns.map((column, index) => (
-            <th key={index}>{column}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            {columns.map((column, colIndex) => {
-              const colSpan = row['colSpan'] || 1;
-              // Проверяем, если текущая ячейка имеет colSpan и она не первая в ряду, пропускаем ее
-              if (colSpan > 1 && colIndex > 0) {
-                return null;
-              }
+    return (
+        <div className='divkaParent'>
+            <div className='divka'>
+                <button className='ComplexTableBtn' onClick={toggleCollapse}>
+                    {isCollapsed ? 'Развернуть Таблицу' : 'Свернуть Таблицу'}
+                </button>
+                <table className="ComplexTable">
+                    <thead>
+                    <tr>
+                        {columns.map((column, index) => (
+                            <th key={index}>{column}</th>
+                        ))}
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {data.slice(0, showRows).map((row, rowIndex) => (
+                        <tr key={rowIndex}>
+                            {columns.map((column, colIndex) => {
+                                const colSpan = row['colSpan'] || 1;
+                                // Проверяем, если текущая ячейка имеет colSpan и она не первая в ряду, пропускаем ее
+                                if (colSpan > 1 && colIndex > 0) {
+                                    return null;
+                                }
 
-              return (
-                <td key={colIndex} colSpan={colSpan}
-                  style={{
-                  textAlign: colSpan === 3 ? 'center' : 'left',
-                  fontWeight: colSpan === 3 ? 'bold' : 'normal',
-                }}>
-                  {Array.isArray(row[column]) ? (
-                    row[column].map((line, lineIndex) => (
-                      <React.Fragment key={lineIndex}>
-                        {lineIndex > 0 && <hr />} {/* Добавляем <hr> после первой строки */}
-                        {line}
-                      </React.Fragment>
-                    ))
-                  ) : (
-                    row[column]
-                  )}
-                </td>
-              );
-            })}
-          </tr>
-        ))}
-      </tbody>
-      </table>
-    </div>
-  );
+                                return (
+                                    <td key={colIndex} colSpan={colSpan}
+                                        style={{
+                                            textAlign: colSpan === 3 ? 'center' : 'left',
+                                            fontWeight: colSpan === 3 ? 'bold' : 'normal',
+                                        }}>
+                                        {Array.isArray(row[column]) ? (
+                                            row[column].map((line, lineIndex) => (
+                                                <React.Fragment key={lineIndex}>
+                                                    {lineIndex > 0 && <hr />} {/* Добавляем <hr> после первой строки */}
+                                                    {line}
+                                                </React.Fragment>
+                                            ))
+                                        ) : (
+                                            row[column]
+                                        )}
+                                    </td>
+                                );
+                            })}
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    );
 };
 
 export default ComplexTable;
