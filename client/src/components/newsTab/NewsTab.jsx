b2a8fd455f964a51290e402b8ea6d1095d9f957e
daulet <dauka2707@gmail.com>
2024-03-05 15:57:06 +0600

    adawd

--- client/src/components/newsTab/NewsTab.jsx
+++ client/src/components/newsTab/NewsTab.jsx
@@ -25,7 +25,7 @@ const NewsTab = ({Width}) => {
   const [settings, setSettings] = useState({
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 2000,
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
