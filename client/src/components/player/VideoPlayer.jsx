b0ce0e71aa10688cd3b537f35ba49e7637d973aa
daulet <dauka2707@gmail.com>
2024-03-09 03:36:24 +0600

    ghjio

--- client/src/components/player/VideoPlayer.jsx
+++ client/src/components/player/VideoPlayer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const VideoPlayer = () => {
   return (
-    <div style={{ width: '100%', margin: '0 auto' }}>
-        <iframe class='sproutvideo-player' src='https://videos.sproutvideo.com/embed/7090d7b41f1ee9c9f9/143d80e3bde17d37?autoPlay=true&amp;playerColor=00a2ff&amp;loop=true' width='640' height='360' frameborder='0' allowfullscreen referrerpolicy='no-referrer-when-downgrade' title='Video Player'></iframe>    </div>
+    <div style={{ margin: '0 auto' }}>
+        <iframe class='sproutvideo-player' src='https://videos.sproutvideo.com/embed/7090d7b41f1ee9c9f9/143d80e3bde17d37?autoPlay=true&amp;playerColor=00a2ff&amp;loop=true' width='640' height='425' frameborder='0' allowfullscreen referrerpolicy='no-referrer-when-downgrade' title='Video Player'></iframe>    </div>
   );
 };
 
