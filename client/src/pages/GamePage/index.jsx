627bc59dd14793ef45c78d3dcfd3e63b47f8b6c3
daulet <dauka2707@gmail.com>
2024-05-06 15:06:32 +0600

    AMLGAme

--- client/src/pages/GamePage/index.jsx
+++ client/src/pages/GamePage/index.jsx
@@ -11,12 +11,12 @@ function GamePage() {
         <div className='WelcomeGameWrapper'>
         <div className='WelcomeLeftPart'>
           <div className='WelcomeLeftPartTop'>
-          <p style={{fontSize:"50px", marginBottom:"50px", marginTop:"120px"}}>Добро пожаловать в</p>
-          <p style={{fontSize:"100px", fontWeight:"800", color:"#1A2751"}}>AML GAME!</p>
+          <p style={{fontSize:"50px", marginBottom:"50px", marginTop:"120px", fontWeight:"500"}}>Добро пожаловать в</p>
+          <p style={{fontSize:"100px", fontWeight:"800", color:"#1A2751", width:"300px"}}>AML GAME!</p>
           </div>
           <div className='WelcomeLeftPartBottom'>
-            <span>МИССИЯ</span>
-            <p>Обучение субъектов финансового мониторинга, предоставляя им возможность получить знания и навыки, необходимые для эффективного выполнения требований законодательства о противодействии легализации доходов, полученных преступным путем (ПОД/ФТ), а также подготовки к различным ситуациям, с которыми они могут столкнуться в реальной жизни.</p>
+            <p style={{padding:"30px",paddingBottom:"10px", fontWeight:"600", color:"#1A2751", fontSize:"21px"}}>МИССИЯ</p>
+            <p style={{padding:"30px"}}>Обучение субъектов финансового мониторинга, предоставляя им возможность получить знания и навыки, необходимые для эффективного выполнения требований законодательства о противодействии легализации доходов, полученных преступным путем (ПОД/ФТ), а также подготовки к различным ситуациям, с которыми они могут столкнуться в реальной жизни.</p>
           </div>
 
         </div>
