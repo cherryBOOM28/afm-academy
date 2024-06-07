591b4c02e3aefe6fcbee6e1b02cd38cd6211e169
Dauka12 <dauka2707@gmail.com>
2024-06-07 12:55:38 +0600

    CARDS

---
+++ client/src/pages/aml-games/components/QuizCards/index.jsx
@@ -0,0 +1,45 @@
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
+import { Button } from '@mui/material';
+import React, { useState } from 'react';
+import './style.scss';
+
+const QuizCard = ({ logo, text }) => {
+    const [flipped, setFlipped] = useState(false);
+    const [displayContent, setDisplayContent] = useState({ logo, text });
+
+    const handleFlip = () => {
+        // Поворот карты
+        setFlipped(!flipped);
+
+        // Задержка для обновления контента, чтобы он не менялся во время видимой части анимации
+        setTimeout(() => {
+            if (flipped) {
+                // Обновление когда карта снова будет в исходном положении
+                setDisplayContent({ logo, text });
+            }
+        }, 400); // Время должно соответствовать продолжительности анимации
+    };
+
+    return (
+        <div className='Quizcard-container'>
+            <div className="Quizcard-wrapper" onClick={handleFlip}>
+                <div className={`Quizcard ${flipped ? 'flipped' : ''}`}>
+                    <div className="Quizcard-face front">
+                        <img src={logo} alt="logo" className="logo large" />
+                    </div>
+                    <div className="Quizcard-face back">
+                        <img src={displayContent.logo} alt="logo" className="logo small" />
+                        <p>{displayContent.text}</p>
+                        <div className="buttons">
+                            <Button startIcon={<CloseIcon />} variant="contained" color="error" className="icon-button" />
+                            <Button startIcon={<CheckIcon />} variant="contained" color="success" className="icon-button" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default QuizCard;
--- client/src/pages/aml-games/game-2/TaskMocks/level_1_2.jsx
+++ client/src/pages/aml-games/game-2/TaskMocks/level_1_2.jsx
@@ -2,7 +2,9 @@ import Divider from '@mui/material/Divider';
 import Sizebox from "../../../../components/courseTemplates/common/Sizebox/index.jsx";
 import AsianMan from "../../assets/asian-man.png";
 import AsianWomen from "../../assets/asian-woman.png";
+import CardAmlLogo from "../../assets/card-aml-logo.svg";
 import WhiteMan from "../../assets/white-man.png";
+import QuizCards from "../../components/QuizCards";
 import VerticalCarousel from '../../components/VerticalCarousel/index.jsx';
 import MessagesPage from "../MessagePage/MessagesPage.tsx";
 
@@ -10,7 +12,11 @@ const cardData = [
     { name: 'Айжан', date: '01.02.1998', id: '**KAZAKHSTAN001**', characterImg: AsianWomen },
     { name: 'Дамир', date: '01.02.1999', id: '**KAZAKHSTAN002**', characterImg: AsianMan },
     { name: 'Дархан', date: '01.02.2000', id: '**KAZAKHSTAN003**', characterImg: WhiteMan }
-  ];
+];
+const quizCardsData = [
+    { id: 1, text: 'Разработку и согласование, внесение изменений и (или) дополнений в ПВК, а также мониторинг реализации и соблюдения ПВК' },
+    { id: 2, text: 'Разработку и согласование, внесение изменений и (или) дополнений в ПВК, а также мониторинг реализации и соблюдения ПВК' },
+];
 
 
 function Level_1_3() {
@@ -29,9 +35,23 @@ function Level_1_3() {
                 </h2>
                 <div className='task1-national-id-card'>
                     <p className='task1-national-id-card-p-text'>Назначьте на должность сотрудника, который наилучшим образом соответствует требованиям ПОД/ФТ и потребностям вашей организации.</p>
-
                     <VerticalCarousel cards={cardData} />
                 </div>
+            </div>
+            
+            <Sizebox height={100} />
+            <Divider sx={{ backgroundColor: "rgba(31, 60, 136, 0.7)" }} />
+            <div>
+                <h2>
+                    Задача 2
+                </h2>
+                <p>После определения правильного кандидата на должность ответственного сотрудника, требуется определить его функции в сфере ПОД/ФТ</p>
+                <Sizebox height={40} />
+                <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                {quizCardsData.map(card => (
+                    <QuizCards key={card.id} logo={CardAmlLogo} text={card.text} />
+                ))}
+        </div>
             </div>
 
         </>
