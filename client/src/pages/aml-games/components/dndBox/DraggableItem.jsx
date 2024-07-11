ea3a6d801dec574c80c78ef09ba14d1823aca517
AdilSE-2112 <97591283+AdilSE-2112@users.noreply.github.com>
2024-07-11 10:07:17 +0500

    F

--- client/src/pages/aml-games/components/dndBox/DraggableItem.jsx
+++ client/src/pages/aml-games/components/dndBox/DraggableItem.jsx
@@ -15,7 +15,6 @@ const DraggableItem = ({ id, name, flag }) => {
       ref={drag}
       style={{
         opacity: isDragging ? 0.5 : 1,
-<<<<<<< HEAD
         padding: "8px",
         border: "1px dashed gray",
         margin: "4px",
@@ -23,7 +22,7 @@ const DraggableItem = ({ id, name, flag }) => {
         fontSize: "13px",
         backgroundColor: "rgba(217, 217, 217, 0.13)",
         borderRadius: "10px",
-        width: flag ? "calc(50% - 86px)": "calc(50% - 8px)",
+        width: flag ? "calc(50% - 75px)": "calc(50% - 8px)",
         boxSizing: "border-box",
         height: flag ? "80px": "50px",
         display: "flex",
@@ -31,23 +30,6 @@ const DraggableItem = ({ id, name, flag }) => {
         justifyContent: "center",
         flexGrow: 0,
         flexShrink: 0,
-=======
-        padding: '8px',
-        border: '1px dashed gray',
-        margin: '4px',
-        textAlign: 'center',
-        fontSize: '13px',
-        backgroundColor: 'rgba(217, 217, 217, 0.13)',
-        borderRadius: '10px',
-        width: 'calc(50% - 8px)', // Уменьшение для двух рядов с учетом отступов
-        boxSizing: 'border-box', // Для корректного учета padding и border
-        height: '50px', // Установим фиксированную высоту
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexGrow: 0, // Отключаем рост
-        flexShrink: 0 // Отключаем сжатие
->>>>>>> 5ae7d26db10087e489734fecb0d29a389cdbfc37
       }}
     >
       {flag ? (
