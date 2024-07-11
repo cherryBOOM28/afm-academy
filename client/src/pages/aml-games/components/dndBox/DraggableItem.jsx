a35b9633c4a0306b32d8eaba767d24fb2b886100
Dauka12 <dauka2707@gmail.com>
2024-07-11 10:10:14 +0600

    border: "1px dashed gray",

--- client/src/pages/aml-games/components/dndBox/DraggableItem.jsx
+++ client/src/pages/aml-games/components/dndBox/DraggableItem.jsx
@@ -15,7 +15,6 @@ const DraggableItem = ({ id, name, flag }) => {
       ref={drag}
       style={{
         opacity: isDragging ? 0.5 : 1,
-<<<<<<< HEAD
         padding: "8px",
         border: "1px dashed gray",
         margin: "4px",
@@ -31,23 +30,6 @@ const DraggableItem = ({ id, name, flag }) => {
         justifyContent: "center",
         flexGrow: 0,
         flexShrink: 0,
-=======
-        padding: '8px',
-        border: '1px dashed gray',
-        margin: '4px',
-        textAlign: 'center',
-        fontSize: '13px',
-        backgroundColor: 'rgba(217, 217, 217, 0.13)',
-        borderRadius: '10px',
-        width: 'calc(50% - 8px)', // Уменьшение для двух рядов с учетом отступов
-        boxSizing: 'border-box', // Для корректного учета padding и border
-        height: '50px', // Установим фиксированную высоту
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexGrow: 0, // Отключаем рост
-        flexShrink: 0 // Отключаем сжатие
->>>>>>> 5ae7d26db10087e489734fecb0d29a389cdbfc37
       }}
     >
       {flag ? (
