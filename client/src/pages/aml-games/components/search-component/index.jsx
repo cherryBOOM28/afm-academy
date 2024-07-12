97ead4bd968d372a19301a40c7205def7625cdd1
Dauka12 <dauka2707@gmail.com>
2024-07-12 17:43:02 +0600

    asdaw

---
+++ client/src/pages/aml-games/components/search-component/index.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import searchImg from '../../assets/search.svg'
+import './style.css'
+
+const SearchComponent = ({ peopleData }) => {
+    const [searchTerm, setSearchTerm] = useState('')
+    const [filteredResults, setFilteredResults] = useState([])
+
+    const handleSearch = (event) => {
+        const value = event.target.value;
+        setSearchTerm(value);
+        const result = peopleData.filter(person => 
+            person.id.includes(value)
+        )
+        setFilteredResults(result)
+    }
+    return (
+        <div className='search-component-wrapper'>
+            <div className='search-component'>
+                <div className='search-component-input'>
+                    <img className='search-component-icon' src={searchImg} alt="" />
+                    <input
+                        className='search-component-text-input'
+                        type="text"
+                        placeholder='ИИН/БИН'
+                        value={searchTerm}
+                        onChange={handleSearch}
+                    />
+                    <button className='search-component-button'>Поиск</button>
+                </div>
+                <div className="search-component-description">
+                    {searchTerm === '' ? (
+                        <p>Публичные должностные лица, их супруги и близкие родственники</p>
+                    ) : (
+                        <ul className="search-component-results">
+                            {filteredResults.length > 0 ? (
+                                filteredResults.map((person, index) => (
+                                    <li key={index} className="search-component-result-item">
+                                        {person.name}: {person.id}
+                                    </li>
+                                ))
+                            ) : (
+                                <p>Результатов не найдено</p>
+                            )}
+                        </ul>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SearchComponent
