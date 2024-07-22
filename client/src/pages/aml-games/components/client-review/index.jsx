3610fdf32c83b3665267f198431b3c7b7b3e04b8
Dauka12 <dauka2707@gmail.com>
2024-07-22 18:13:52 +0600

    awdawd

--- client/src/pages/aml-games/components/client-review/index.jsx
+++ client/src/pages/aml-games/components/client-review/index.jsx
@@ -27,38 +27,40 @@ const ClientReview = ({ clients }) => {
     const { description, img, fullName } = clients[currentClientIndex];
 
     return (
-        <div className="client-review-container">
-            <div className="client-review-res">
-                <div className='client-review-description-container'>
-                    <p className='client-review-description'>{description}</p>
-                    <div className="client-review-buttons">
-                        <NameList peopleData={[{
-                            name: fullName,
-                            id: ''
-                        }]}/>
+        <div style={{width:"100%", display:"flex", justifyContent:"center"}}>
+            <div className="client-review-container">
+                <div className="client-review-res">
+                    <div className='client-review-description-container'>
+                        <p className='client-review-description'>{description}</p>
+                        <div className="client-review-buttons">
+                            <NameList peopleData={[{
+                                name: fullName,
+                                id: ''
+                            }]} />
+                        </div>
+                    </div>
+                    <div className='fullInfo-client'>
+                        <h3>Клиент {currentClientIndex + 1}:</h3>
+                        <div className='img-container-review'>
+                            <img src={img} alt={fullName} />
+                        </div>
+                        <p>{fullName}</p>
                     </div>
                 </div>
-                <div className='fullInfo-client'>
-                    <h3>Клиент {currentClientIndex + 1}:</h3>
-                    <div className='img-container-review'>
-                        <img src={img} alt={fullName} />
-                    </div>
-                    <p>{fullName}</p>
-                </div>
-            </div>
 
-            <div style={{display:"flex", alignItems:"center"}}>
-                <div className="client-review-stepper">
-                    {clients.map((_, index) => (
-                        <span
-                            key={index}
-                            className={index === currentClientIndex ? 'active' : ''}
-                        ></span>
-                    ))}
-                </div>
-                <div className="client-review-navigation">
-                    <button className='client-review-navigation-previous' onClick={prevClient} disabled={currentClientIndex === 0}>Назад</button>
-                    <button className='client-review-navigation-next' onClick={nextClient} disabled={currentClientIndex === clients.length - 1}>Далее</button>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                    <div className="client-review-stepper">
+                        {clients.map((_, index) => (
+                            <span
+                                key={index}
+                                className={index === currentClientIndex ? 'active' : ''}
+                            ></span>
+                        ))}
+                    </div>
+                    <div className="client-review-navigation">
+                        <button className='client-review-navigation-previous' onClick={prevClient} disabled={currentClientIndex === 0}>Назад</button>
+                        <button className='client-review-navigation-next' onClick={nextClient} disabled={currentClientIndex === clients.length - 1}>Далее</button>
+                    </div>
                 </div>
             </div>
         </div>
