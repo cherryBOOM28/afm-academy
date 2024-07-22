e9ed11f486747d5a4a530c9d4104c9879ca5a37c
AdilSE-2112 <97591283+AdilSE-2112@users.noreply.github.com>
2024-07-22 16:35:45 +0500

    F

---
+++ client/src/pages/aml-games/game-2/TaskMocks/level_3_1/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Sizebox from "../../../../../components/courseTemplates/common/Sizebox";
+import DossierComponent from "../../../components/dossier/index";
+
+function Level_3_1() {
+  return (
+    <>
+      <h2>Задача 1</h2>
+      <Sizebox height={40} />
+        <DossierComponent />
+
+    </>
+  );
+}
+
+export default Level_3_1;
